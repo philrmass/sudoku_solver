aa86ca89a4748798f6727788d058a83f02ae43d6
Phil Mass <philrmass@gmail.com>
2019-01-21 21:56:52 -0800

    add plan to module

--- lib/sudoku_solver.ex
+++ lib/sudoku_solver.ex
@@ -1,7 +1,19 @@
+# change possibles to MapSet
+# puzzle_string :: string
+# possibles_string :: string
+# add first tests, run them
+# same_row(index0, index1) :: boolean
+# same_column(index0, index1) :: boolean
+# same_box(index0, index1) :: boolean
+# same_group(index0, index1) :: boolean
+# group_impossibles(index0, index1) :: MapSet
 defmodule SudokuSolver do
   def main(args) do
-    puzzle = args |> parse_args |> load 
+    {:ok, name, puzzle} = args |> parse_args |> load 
+    possibles = to_possibles(puzzle)
+    IO.puts name
     IO.inspect puzzle
+    IO.inspect possibles
   end
 
   defp parse_args(args) do
@@ -34,15 +46,23 @@ defmodule SudokuSolver do
   end
 
   defp load_puzzle(name, lines) do
-    values = load_puzzle_values(lines);
+    values = load_puzzle_values(lines)
     status = if length(values) == 81, do: :ok, else: :error
-    {status, name, List.to_tuple(values)}
+    {status, name, values}
   end
 
   def load_puzzle_values(lines) do
     lines
     |> Enum.join("")
     |> String.split("", trim: true)
-    |> Enum.map(fn x -> String.to_integer(x) end);
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+
+  def value_to_possibles(value) do
+    if value == 0, do: Enum.to_list(1..9), else: [value]
+  end
+
+  def to_possibles(puzzle) do
+    Enum.map(puzzle, &value_to_possibles/1)
   end
 end
